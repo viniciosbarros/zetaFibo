2fcb0e847a3e2d0d8d99d603a77af95223f6c81d
Vinicios Barros <vinicios.barros@ieee.org>
2014-08-11 10:41:18 -0300

    zetafibo: last commit was broking compilation

--- zetaFibo.c
+++ zetaFibo.c
@@ -9,18 +9,12 @@
 #include <time.h>
 #include <sys/time.h>
 
-#define BASE	10
+#define BASE  10
 #define ESPERTO
 
-#define MEMORIA 100000000
-
-int vector[MEMORIA];
-int vector_final[MEMORIA];
-int vector_indice[MEMORIA];
-
-// primeiros termos da serie
-vector[0]=0;
-vector[1]=1;
+int vector[100];
+int vector_final[100];
+int vector_indice[100];
 
 
 int fibonacci(int n);
@@ -30,7 +24,7 @@ int main(int argc, char *argv[])
 {
 
   long limit,
-  ret = -1, i, max, j = 0, last_value,
+  ret, i, j = 0, last_value,
   num_rec = -1;
   struct timeval tv1,tv2;
 
@@ -38,21 +32,20 @@ int main(int argc, char *argv[])
   vector[0]=0;
   vector[1]=1;
 
-	if( argc < 2 ){
-		printf("ERROR: Must be one argument value \n Ex: %s 15 \n",argv[0]);
-		return 1;
-	}
+  if( argc < 2 ){
+    printf("ERROR: Must be one argument value \n Ex: %s 15 \n",argv[0]);
+    return 1;
+  }
     
     
-  max = strtol(argv[1], NULL ,BASE);
+  limit = strtol(argv[1], NULL ,BASE);
 
   gettimeofday(&tv1, NULL);
 
-for (limit = 1; limit <= max; limit++){
-
-  while(vector[num_rec] < limit){
+  while(ret < limit){
     num_rec++;
     vector[num_rec] = fibonacci(num_rec);
+    ret =+ vector[num_rec];
   }
 
   gettimeofday(&tv2, NULL);
@@ -70,7 +63,7 @@ for (limit = 1; limit <= max; limit++){
 
   for (i=num_rec-1; i >= 0; --i){
     last_value = last_value + vector[i];
-    if (last_value >= limit){
+    if (last_value > limit){
       last_value = last_value - vector[i];
     }
     else{
@@ -79,7 +72,6 @@ for (limit = 1; limit <= max; limit++){
       j++;
     }
   }
-}
 
   gettimeofday(&tv2, NULL);
   printf ("Total time = %f seconds\n",
@@ -87,13 +79,8 @@ for (limit = 1; limit <= max; limit++){
     (double) (tv2.tv_sec - tv1.tv_sec));
 
 
-  for (i = 0; i < j; i++){
+  for (i = 0; i < j; i++)
     printf("\nzf(%d)= %d ", vector_indice[i], vector_final[i]);
-    if (vector_indice[i] == 0 && vector_final[i] == 0 )
-      printf("\n");    
-  }
-
-  j=0;
 
     printf("\n");
     return 0;
